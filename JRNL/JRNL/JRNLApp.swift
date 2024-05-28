0b9b9faf42d838b681e76b07b1a0a71fa0e2a29d
Cha, Jiyong <wldyd2113@gmail.com>
2024-05-28 14:37:38 +0900

    New Project Create JRNL

---
+++ JRNL/JRNL/JRNLApp.swift
@@ -0,0 +1,17 @@
+//
+//  JRNLApp.swift
+//  JRNL
+//
+//  Created by 차지용 on 5/28/24.
+//
+
+import SwiftUI
+
+@main
+struct JRNLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
