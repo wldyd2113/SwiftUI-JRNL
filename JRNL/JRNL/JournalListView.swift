34d8d822dc2f783d5b203782f70285bada1ec672
Cha, Jiyong <wldyd2113@gmail.com>
2024-05-28 17:08:36 +0900

    Tab 뷰 추가

---
+++ JRNL/JRNL/JournalListView.swift
@@ -0,0 +1,18 @@
+//
+//  JournalListView.swift
+//  JRNL
+//
+//  Created by 차지용 on 5/28/24.
+//
+
+import SwiftUI
+
+struct JournalListView: View {
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    JournalListView()
+}
---
+++ JRNL/JRNL/MapView.swift
@@ -0,0 +1,18 @@
+//
+//  MapView.swift
+//  JRNL
+//
+//  Created by 차지용 on 5/28/24.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text("Map")
+    }
+}
+
+#Preview {
+    MapView()
+}
