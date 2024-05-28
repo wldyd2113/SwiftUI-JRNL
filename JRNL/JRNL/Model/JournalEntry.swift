2641defbf8dcd119f8d0fda3203e56d224ecd056
Cha, Jiyong <wldyd2113@gmail.com>
2024-05-28 15:09:25 +0900

    JournalEntry

--- JRNL/JRNL/Model/JournalEntry.swift
+++ JRNL/JRNL/Model/JournalEntry.swift
@@ -7,24 +7,24 @@
 
 import Foundation
 
-//class JournalEntry {
-//    let dateString: String
-//    let rating: Int
-//    let entryTitle: String
-//    let entryVody: String
-//    let photoData: Data?
-//    let latitude: Double?
-//    let longitoude: Double?
-//    
-//    init(dateString: String, rating: Int, entryTitle: String, entryVody: String, photoData: Data?, latitude: Double?, longitoude: Double?) {
-//        self.dateString = dateString
-//        self.rating = rating
-//        self.entryTitle = entryTitle
-//        self.entryVody = entryVody
-//        self.photoData = photoData
-//        self.latitude = latitude
-//        self.longitoude = longitoude
-//    }
-//    
-//    
-//}
+class JournalEntry {
+    let dateString: String
+    let rating: Int
+    let entryTitle: String
+    let entryVody: String
+    let photoData: Data?
+    let latitude: Double?
+    let longitoude: Double?
+    
+    init(dateString: String, rating: Int, entryTitle: String, entryVody: String, photoData: Data?, latitude: Double?, longitoude: Double?) {
+        self.dateString = dateString
+        self.rating = rating
+        self.entryTitle = entryTitle
+        self.entryVody = entryVody
+        self.photoData = photoData
+        self.latitude = latitude
+        self.longitoude = longitoude
+    }
+    
+    
+}
