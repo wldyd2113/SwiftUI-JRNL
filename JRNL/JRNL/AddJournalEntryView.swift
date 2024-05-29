c52819d0d2cca0ded954198c8ccea9c2125c42d3
Cha, Jiyong <wldyd2113@gmail.com>
2024-05-29 09:34:52 +0900

    위치 정보 추가

--- JRNL/JRNL/AddJournalEntryView.swift
+++ JRNL/JRNL/AddJournalEntryView.swift
@@ -6,7 +6,7 @@
 //
 
 import SwiftUI
-
+import CoreLocation
 struct RatingView: View {
     @Binding var rating: Int
     
@@ -23,12 +23,41 @@ struct RatingView: View {
     }
 }
 
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+    private let manager = CLLocationManager()
+    @Published var location: CLLocation?
+
+    override init() {
+        super.init()
+        manager.delegate = self
+        manager.requestAlwaysAuthorization()
+        manager.desiredAccuracy = kCLLocationAccuracyKilometer
+    }
+
+    func requestLocation() {
+        manager.requestLocation()
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            self.location = location
+        }
+    }
+
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: any Error) {
+        print("Failed: \(error.localizedDescription)")
+    }
+}
+
 struct AddJournalEntryView: View {
     @Environment(\.dismiss) var dismiss
     @Environment(\.modelContext) var modelContext
     
+    @StateObject private var locationManager = LocationManager()
     @State private var rating = 0
     @State private var isGetLocationOn = false
+    @State private var locationLabel = "Get Location"
+    @State private var currentLocation: CLLocation?
     @State private var entryTitle = ""
     @State private var entryBody = ""
     
@@ -39,15 +68,34 @@ struct AddJournalEntryView: View {
                     RatingView(rating: $rating)
                         .frame(maxWidth: .infinity, alignment: .center)
                 }
+                
                 Section(header: Text("Location")) {
                     Toggle("Get Location", isOn: $isGetLocationOn)
                 }
+                
                 Section(header: Text("Title")) {
                     TextField("Enter title", text: $entryTitle)
+                        .onChange(of: isGetLocationOn) {
+                            if isGetLocationOn {
+                                locationLabel = "Get Location..."
+                                locationManager.requestLocation()
+                            }
+                            else {
+                                locationLabel = "Get Location"
+                            }
+                        }
+                        .onReceive(locationManager.$location) { location in
+                            if isGetLocationOn {
+                                currentLocation = location
+                                locationLabel = "Done"
+                            }
+                        }
                 }
+                
                 Section(header: Text("Body")) {
                     TextEditor( text: $entryBody)
                 }
+                
                 Section(header: Text("PHOTO")) {
                     
                 }
@@ -64,7 +112,7 @@ struct AddJournalEntryView: View {
                     ToolbarItem(placement: .topBarTrailing) {
                         Button("Save") {
                             dismiss()
-                            let journalEntry = JournalEntry(rating: 3, entryTitle: entryTitle, entryBody: entryBody, latitude: nil, longitude: nil)
+                            let journalEntry = JournalEntry(rating: 3, entryTitle: entryTitle, entryBody: entryBody, latitude: currentLocation?.coordinate.latitude, longitude: currentLocation?.coordinate.longitude)
                             modelContext.insert(journalEntry)
                         }
                     }
